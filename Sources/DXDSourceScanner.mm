8d81970b8265b0fa7bd18298d861c204d71d849a
Michel Fortin <michel.fortin@michelf.com>
2008-05-30 21:36:23 -0400

    Updated copyright year.

--- Sources/DXDSourceScanner.mm
+++ Sources/DXDSourceScanner.mm
@@ -1,6 +1,6 @@
 
 // D for Xcode: Source Scanner
-// Copyright (C) 2007  Michel Fortin
+// Copyright (C) 2007-2008  Michel Fortin
 //
 // D for Xcode is free software; you can redistribute it and/or modify it 
 // under the terms of the GNU General Public License as published by the Free 
